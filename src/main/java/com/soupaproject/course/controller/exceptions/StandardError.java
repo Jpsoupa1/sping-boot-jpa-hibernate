4a554de45db207ad8088351d87acdb8f7c6ed193
Jpsoupa1 <joaopedrosoupa@sempreceub.com>
2025-12-05 23:52:01 -0300

    Exception handling - findById

--- src/main/java/com/soupaproject/course/controller/exceptions/StandardError.java
+++ src/main/java/com/soupaproject/course/controller/exceptions/StandardError.java
@@ -2,7 +2,11 @@ package com.soupaproject.course.controller.exceptions;
 
 import java.time.Instant;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 public class StandardError {
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant timestamp;
     private Integer status;
     private String error;
